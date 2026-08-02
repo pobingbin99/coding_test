e2a6fe8d7a0bbab250eaa56f8eafa4325f665ec5
pobingbing <leedmswls@gmail.com>
2026-08-03 08:54:12 +0900

    [D2] Title: 어디에 단어가 들어갈 수 있을까, Time: 118 ms, Memory: 30,588 KB -BaekjoonHub

---
+++ SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+class Solution {
+	public static void main(String args[]) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		
+		for(int test_case = 1; test_case <= T; test_case++) {
+			int N = sc.nextInt();
+            int K = sc.nextInt();
+            
+            int[][] arr = new int[N][N];
+            for (int i = 0; i < N; i++) {
+            	for (int j = 0; j < N; j++) {
+                	arr[i][j] = sc.nextInt();
+                }
+            }
+            
+            int ans = 0;
+            
+            for (int i = 0; i < N; i++) {
+                int count = 0;
+            	for (int j = 0; j < N; j++) {
+                    if (arr[i][j] == 1) count++;
+                    else {
+                        if (count == K) ans++;
+                    	count = 0;
+                    }
+                }
+                if (count == K) ans++;
+            }
+            
+            for (int i = 0; i < N; i++) {
+                int count = 0;
+            	for (int j = 0; j < N; j++) {
+                    if (arr[j][i] == 1) count++;
+                    else {
+                        if (count == K) ans++;
+                    	count = 0;
+                    }
+                }
+                if (count == K) ans++;
+            }
+            System.out.println("#" + test_case + " " + ans);
+		}
+	}
+}

63e84f8ca81613e5de5f75a3de37c14fca0df63e
pobingbing <leedmswls@gmail.com>
2026-08-03 09:14:35 +0900

    [D2] Title: 어디에 단어가 들어갈 수 있을까, Time: 120 ms, Memory: 30,820 KB -BaekjoonHub

--- SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.java
+++ SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.java
@@ -5,43 +5,43 @@ class Solution {
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
 		
-		for(int test_case = 1; test_case <= T; test_case++) {
+		for(int tc = 1; tc <= T; tc++) {
 			int N = sc.nextInt();
-            int K = sc.nextInt();
+      int K = sc.nextInt();
             
-            int[][] arr = new int[N][N];
-            for (int i = 0; i < N; i++) {
-            	for (int j = 0; j < N; j++) {
-                	arr[i][j] = sc.nextInt();
-                }
+      int[][] arr = new int[N][N];
+	    for (int i = 0; i < N; i++) {
+			  for (int j = 0; j < N; j++) {
+		      arr[i][j] = sc.nextInt();
+        }
+      }
+            
+      int ans = 0;
+            
+      for (int i = 0; i < N; i++) {
+	      int count = 0;
+        for (int j = 0; j < N; j++) {
+	        if (arr[i][j] == 1) count++;
+          else {
+	          if (count == K) ans++;
+		          count = 0;
+	          }
+		      }
+	      if (count == K) ans++;
+      }
+            
+      for (int i = 0; i < N; i++) {
+		    int count = 0;
+	      for (int j = 0; j < N; j++) {
+		      if (arr[j][i] == 1) count++;
+	        else {
+	          if (count == K) ans++;
+	            count = 0;
             }
-            
-            int ans = 0;
-            
-            for (int i = 0; i < N; i++) {
-                int count = 0;
-            	for (int j = 0; j < N; j++) {
-                    if (arr[i][j] == 1) count++;
-                    else {
-                        if (count == K) ans++;
-                    	count = 0;
-                    }
-                }
-                if (count == K) ans++;
-            }
-            
-            for (int i = 0; i < N; i++) {
-                int count = 0;
-            	for (int j = 0; j < N; j++) {
-                    if (arr[j][i] == 1) count++;
-                    else {
-                        if (count == K) ans++;
-                    	count = 0;
-                    }
-                }
-                if (count == K) ans++;
-            }
-            System.out.println("#" + test_case + " " + ans);
+          }
+	      if (count == K) ans++;
+	    }
+	    System.out.println("#" + tc + " " + ans);
 		}
 	}
 }

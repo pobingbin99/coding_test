e0b987fb39166e39bb9827a3356d870f00f88496
pobingbing <leedmswls@gmail.com>
2026-08-04 08:54:20 +0900

    [D2] Title: 두 개의 숫자열, Time: 107 ms, Memory: 26,496 KB -BaekjoonHub

---
+++ SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+
+class Solution {
+	public static void main(String args[]) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+
+		for(int tc = 1; tc <= T; tc++) {
+			int N = sc.nextInt();
+            int M = sc.nextInt();
+            
+            int[] arrShort = new int[N];
+            int[] arrLong = new int[M];
+            
+            for (int i = 0; i < N; i++) {
+                arrShort[i] = sc.nextInt();
+            }
+            for (int i = 0; i < M; i++) {
+                arrLong[i] = sc.nextInt();
+            }
+            
+            if (N > M) {
+                int temp = N;
+                N = M;
+                M = temp;
+
+                int[] tempArr = arrShort;
+                arrShort = arrLong;
+                arrLong = tempArr;
+            }
+            
+            int max = 0;
+            for (int i = 0; i <= (M-N); i++) {
+             	int sum = 0;
+                for (int j = 0; j < N; j++) {
+                	sum += arrShort[j] * arrLong[j + i];
+                }
+                max = Math.max(max, sum);
+            }
+            
+            System.out.println("#" + tc + " " + max);
+		}
+	}
+}

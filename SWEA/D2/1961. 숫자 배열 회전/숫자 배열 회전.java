b38cd37aa12ad5b457e89102c76be3fe341aea0c
pobingbing <leedmswls@gmail.com>
2026-08-03 11:10:54 +0900

    [D2] Title: 숫자 배열 회전, Time: 116 ms, Memory: 27,648 KB -BaekjoonHub

---
+++ SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+class Solution {
+	public static void main(String args[]) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+
+		for (int tc = 1; tc <= T; tc++) {
+			int N = sc.nextInt();
+            
+            int[][] arr = new int[N][N];
+            for (int i = 0; i < N; i++) {
+             	for (int j = 0; j < N; j++) {
+                 	arr[i][j] = sc.nextInt();
+                }
+            }
+            
+            System.out.println("#" + tc);
+            for (int i = 0; i <N; i++) {
+                for (int j = N-1; j >= 0; j--) {
+                    System.out.print(arr[j][i]);
+                }
+                System.out.print(" ");
+                
+                for (int j = N-1; j >= 0; j--) {
+                	System.out.print(arr[N-1-i][j]);
+                }
+                System.out.print(" ");
+                
+             	for (int j = 0; j < N; j++) {
+                	System.out.print(arr[j][N-1-i]);
+                }
+                System.out.println();
+            }
+ 		}
+	}
+}
